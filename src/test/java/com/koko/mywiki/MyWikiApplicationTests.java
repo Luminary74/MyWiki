b265c2502671dc59c323030cfbd71c38b6c44433
Aur4r0 <aur4r0@gmail.com>
2025-05-31 22:29:11 +0800

    2-2 SpringBoot Init

---
+++ src/test/java/com/koko/mywiki/MyWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koko.mywiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
