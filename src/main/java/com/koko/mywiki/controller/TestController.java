10b6da0f83fd7e872fed36d2ffe0f57b10b5b7fe
Aur4r0 <aur4r0@gmail.com>
2025-05-31 23:39:06 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/koko/mywiki/controller/TestController.java
+++ src/main/java/com/koko/mywiki/controller/TestController.java
@@ -1,17 +1,19 @@
 package com.koko.mywiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
