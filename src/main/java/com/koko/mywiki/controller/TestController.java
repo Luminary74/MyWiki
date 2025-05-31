7e6d3dab6e3b602b48d686a6027c5a745e9b705e
Aur4r0 <aur4r0@gmail.com>
2025-05-31 23:24:02 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/koko/mywiki/controller/TestController.java
+++ src/main/java/com/koko/mywiki/controller/TestController.java
@@ -4,11 +4,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class TestController {
 
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post: " + name ;
     }
 }
