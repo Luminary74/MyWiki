7e6cea72ec9c8e5c6cd9d65ee725ff04f6cd32a6
Aur4r0 <aur4r0@gmail.com>
2025-06-02 00:10:22 +0800

    6-9 增加雪花算法工具类，完成电子书新增功能

--- src/main/java/com/koko/mywiki/service/EbookService.java
+++ src/main/java/com/koko/mywiki/service/EbookService.java
@@ -10,6 +10,7 @@ import com.koko.mywiki.req.EbookSaveReq;
 import com.koko.mywiki.resp.EbookResp;
 import com.koko.mywiki.resp.PageResp;
 import com.koko.mywiki.until.CopyUtil;
+import com.koko.mywiki.until.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,9 @@ public class EbookService {
 
     @Resource
     private EbookMapper ebookMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
 
     public PageResp<EbookResp> list(EbookQueryReq req) {
         EbookExample ebookExample = new EbookExample();
@@ -64,6 +68,7 @@ public class EbookService {
         Ebook ebook = CopyUtil.copy(req, Ebook.class);
         if (ObjectUtils.isEmpty(ebook.getId())) {
             // 新增
+            ebook.setId(snowFlake.nextId());
             ebookMapper.insert(ebook);
         }else {
             // 更新
