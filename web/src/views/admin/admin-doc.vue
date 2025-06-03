e7bdb49631516dd30eb2cc578d5b532044a02ee0
Aur4r0 <aur4r0@gmail.com>
2025-06-03 16:14:26 +0800

    8-6 删除某一个文档时，增加二次确认

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -89,11 +89,12 @@
 </template>
 
 <script lang="ts">
-  import {defineComponent, onMounted, ref} from 'vue';
+import {createVNode, defineComponent, onMounted, ref} from 'vue';
   import axios from 'axios';
-  import { message } from 'ant-design-vue';
+import {message, Modal} from 'ant-design-vue';
   import { Tool } from "@/util/tool";
   import {useRoute} from "vue-router";
+import {ExclamationCircleOutlined} from "@ant-design/icons-vue";
 
   export default defineComponent({
     name: 'AdminDoc',
@@ -171,6 +172,7 @@
       const doc = ref({});
       const modalVisible = ref(false);
       const modalLoading = ref(false);
+
       const handleModalOk = () => {
         modalLoading.value = true;
         axios.post("/doc/save", doc.value).then((response) => {
@@ -219,9 +221,9 @@
         }
       };
 
-      // const deleteIds: Array<string> = [];
-      // const deleteNames: Array<string> = [];
-      const ids: Array<string> = [];
+      const deleteIds: Array<string> = [];
+      const deleteNames: Array<string> = [];
+      //const ids: Array<string> = [];
       /**
        * 查找整根树枝
        */
@@ -235,9 +237,9 @@
             console.log("delete", node);
             // 将目标ID放入结果集ids
             // node.disabled = true;
-            ids.push(id);
-            // deleteIds.push(id);
-            // deleteNames.push(node.name);
+            // ids.push(id);
+            deleteIds.push(id);
+            deleteNames.push(node.name);
 
             // 遍历所有子节点
             const children = node.children;
@@ -293,13 +295,25 @@
        * 删除
        */
       const handleDelete = (id: number) => {
-        getDeleteIds(level1.value,id);
-        axios.delete("/doc/delete/" + ids.join(",")).then((response) => {
-          const data = response.data; // data = commonResp
-          if (data.success) {
-            // 重新加载列表
-            handleQuery();
-          }
+        deleteIds.length = 0;
+        deleteNames.length = 0;
+        getDeleteIds(level1.value, id);
+        Modal.confirm({
+          title: '重要提醒',
+          icon: createVNode(ExclamationCircleOutlined),
+          content: '将删除：【' + deleteNames.join("，") + "】删除后不可恢复，确认删除？",
+          onOk() {
+            // console.log(ids)
+            axios.delete("/doc/delete/" + deleteIds.join(",")).then((response) => {
+              const data = response.data; // data = commonResp
+              if (data.success) {
+                // 重新加载列表
+                handleQuery();
+              } else {
+                message.error(data.message);
+              }
+            });
+          },
         });
       };
 
