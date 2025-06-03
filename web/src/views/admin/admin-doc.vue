820a3867ba4db9021c6992ac818a545740e662ef
Aur4r0 <aur4r0@gmail.com>
2025-06-03 16:58:24 +0800

    8-9 文档管理布局美化 表格紧凑 按钮变小 表单变大 页面打开时就初始化富文本框，z-index变小

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -24,13 +24,14 @@
               :data-source="level1"
               :pagination="false"
               :loading="loading"
+              size="small"
           >
-            <template #cover="{ text: cover }">
-              <img v-if="cover" :src="cover" alt="avatar"/>
+            <template #name="{ text, record }">
+              {{record.sort}} {{text}}
             </template>
             <template v-slot:action="{ text, record }">
               <a-space size="small">
-                <a-button type="primary" @click="edit(record)">
+                <a-button type="primary" @click="edit(record)" size="small">
                   编辑
                 </a-button>
                 <a-popconfirm
@@ -39,7 +40,7 @@
                     cancel-text="否"
                     @confirm="handleDelete(record.id)"
                 >
-                  <a-button type="danger">
+                  <a-button type="danger" size="small">
                     删除
                   </a-button>
                 </a-popconfirm>
@@ -48,11 +49,20 @@
           </a-table>
         </a-col>
         <a-col :span="16">
-          <a-form :model="doc" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
-            <a-form-item label="名称">
-              <a-input v-model:value="doc.name" />
+          <p>
+            <a-form layout="inline" :model="param">
+              <a-form-item>
+                <a-button type="primary" @click="handleSave()">
+                  保存
+                </a-button>
+              </a-form-item>
+            </a-form>
+          </p>
+          <a-form :model="doc" layout="vertical">
+            <a-form-item>
+              <a-input v-model:value="doc.name" placeholder="名称"/>
             </a-form-item>
-            <a-form-item label="父文档">
+            <a-form-item>
               <a-tree-select
                   v-model:value="doc.parent"
                   style="width: 100%"
@@ -64,16 +74,15 @@
               >
               </a-tree-select>
             </a-form-item>
-            <a-form-item label="顺序">
-              <a-input v-model:value="doc.sort" />
+            <a-form-item>
+              <a-input v-model:value="doc.sort" placeholder="顺序"/>
             </a-form-item>
-            <a-form-item label="内容">
+            <a-form-item>
               <div id="content"></div>
             </a-form-item>
           </a-form>
         </a-col>
       </a-row>
-
     </a-layout-content>
   </a-layout>
 
@@ -115,16 +124,8 @@
       const columns = [
         {
           title: '名称',
-          dataIndex: 'name'
-        },
-        {
-          title: '父文档',
-          key: 'parent',
-          dataIndex: 'parent'
-        },
-        {
-          title: '顺序',
-          dataIndex: 'sort'
+          dataIndex: 'name',
+          slots: { customRender: 'name' }
         },
         {
           title: 'Action',
@@ -173,7 +174,8 @@
       const modalVisible = ref(false);
       const modalLoading = ref(false);
       const editor = new E('#content');
-      const handleModalOk = () => {
+      editor.config.zIndex = 0;
+      const handleSave = () => {
         modalLoading.value = true;
         axios.post("/doc/save", doc.value).then((response) => {
           modalLoading.value = false;
@@ -273,11 +275,6 @@
 
         // 为选择树添加一个"无"
         treeSelectData.value.unshift({id: 0, name: '无'});
-        setTimeout(function () {
-          editor.create();
-        },100)
-
-
 
       };
 
@@ -295,10 +292,6 @@
 
         // 为选择树添加一个"无"
         treeSelectData.value.unshift({id: 0, name: '无'});
-
-        setTimeout(function () {
-          editor.create();
-        },100)
 
       };
 
@@ -330,6 +323,8 @@
 
       onMounted(() => {
         handleQuery();
+        editor.create();
+
       });
 
       return {
@@ -349,7 +344,7 @@
         doc,
         modalVisible,
         modalLoading,
-        handleModalOk,
+        handleSave,
 
 
     }
