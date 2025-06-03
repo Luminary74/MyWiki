0b4dc3c6ec4fb0e72b5337a392396166c552818d
Aur4r0 <aur4r0@gmail.com>
2025-06-04 00:59:00 +0800

    9-9 集成redis，登录成功后将登录信息放入redis中

--- src/main/java/com/koko/mywiki/controller/UserController.java
+++ src/main/java/com/koko/mywiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.koko.mywiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.koko.mywiki.req.UserLoginReq;
 import com.koko.mywiki.req.UserQueryReq;
 import com.koko.mywiki.req.UserRestPasswordeReq;
@@ -9,19 +10,32 @@ import com.koko.mywiki.resp.PageResp;
 import com.koko.mywiki.resp.UserLoginResp;
 import com.koko.mywiki.resp.UserQueryResp;
 import com.koko.mywiki.service.UserService;
+import com.koko.mywiki.until.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 
 public class UserController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Resource
     private UserService userService;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req) {
@@ -59,6 +73,12 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token:{}，并放入redis中", token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
+
         resp.setContent(userLoginResp);
         return resp;
     }
--- src/main/java/com/koko/mywiki/resp/UserLoginResp.java
+++ src/main/java/com/koko/mywiki/resp/UserLoginResp.java
@@ -1,14 +1,21 @@
 package com.koko.mywiki.resp;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-
 public class UserLoginResp {
     private Long id;
 
     private String loginName;
 
     private String name;
+
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 
     public Long getId() {
         return id;
@@ -36,14 +43,11 @@ public class UserLoginResp {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", loginName=").append(loginName);
-        sb.append(", name=").append(name);
-        sb.append("]");
-        return sb.toString();
+        return "UserLoginResp{" +
+                "id=" + id +
+                ", loginName='" + loginName + '\'' +
+                ", name='" + name + '\'' +
+                ", token='" + token + '\'' +
+                '}';
     }
 }
