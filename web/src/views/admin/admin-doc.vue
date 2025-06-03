5801e7ec18381fc9cf053b2765e7145115fb3e75
Aur4r0 <aur4r0@gmail.com>
2025-06-03 17:55:25 +0800

    8-11 增加保存文档成功提示 编辑和新增的时候，先清空富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -184,8 +184,9 @@
           modalLoading.value = false;
           const data = response.data; // data = commonResp
           if (data.success) {
-            modalVisible.value = false;
-            modalLoading.value = false;
+            // modalVisible.value = false;
+            // modalLoading.value = false;
+            message.success("保存成功！")
 
             // 重新加载列表
             handleQuery();
@@ -283,6 +284,8 @@
        * 编辑
        */
       const edit = (record: any) => {
+        // 清空富文本框
+        editor.txt.html("");
         modalVisible.value = true;
         doc.value = Tool.copy(record);
         handleQueryContent();
@@ -300,7 +303,8 @@
        * 新增
        */
       const add = () => {
-
+        // 清空富文本框
+        editor.txt.html("");
         modalVisible.value = true;
         doc.value = {
           ebookId: route.query.ebookId
