d096b4dffe2b86983e8452b31020351d52e0b7f7
Aur4r0 <aur4r0@gmail.com>
2025-06-04 12:37:16 +0800

    9-11 前后端增加登录拦截 修复BUG：前后端存储token统一用String类型

--- src/main/java/com/koko/mywiki/config/SpringMvcConfig.java
+++ src/main/java/com/koko/mywiki/config/SpringMvcConfig.java
@@ -1,21 +1,35 @@
-//package com.koko.mywiki.config;
-//
-//import com.koko.mywiki.interceptor.LogInterceptor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import javax.annotation.Resource;
-//
-//
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//
-//    @Resource
-//    LogInterceptor logInterceptor;
-//
-//    public void addInterceptors(InterceptorRegistry registry) {
+package com.koko.mywiki.config;
+
+import com.koko.mywiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    //    @Resource
+//    LogInterceptor logInterceptor;//配置拦截器
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(logInterceptor)
-//                .addPathPatterns("/**").excludePathPatterns("/login");
-//    }
-//}
+//                .addPathPatterns("/**");
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**",
+                        "/ebook-snapshot/**"
+                );
+    }
+}
