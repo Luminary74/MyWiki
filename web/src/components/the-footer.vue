770d89157dc226263ee5c57a2409694264e909f4
Aur4r0 <aur4r0@gmail.com>
2025-06-04 11:52:57 +0800

    9-10 使用vuex保存用户信息，方便其他组件获取登陆信息。遗留问题，刷新后，值没有了

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,13 +1,22 @@
 <template>
   <a-layout-footer style="text-align: center">
+    欢迎：{{user.name}}
     MyWiki ©2024 Created by KOKO
   </a-layout-footer>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
+import {computed, defineComponent} from 'vue';
+import store from "@/store";
 
   export default defineComponent({
     name: 'the-footer',
+    setup(){
+      const user = computed(() => store.state.user);
+
+      return{
+        user
+      }
+    }
 });
 </script>
